5c920842ff7d08c49eaa5a60c350ec0d14b5e3b2
LUDMILA UMA GRÜN <49379260@ort>
2025-09-26 15:50:26 -0300

    Hola

---
+++ PrimerProyecto/Models/BD.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace Proyecto.Models
+{
+    public static class Bd
+    {
+    private static string _connectionString = @"A-PHZ2-CIDI-32";
+
+    }
+
+    public static Categoria ObtenerCategorias(){
+
+    }
+}
+   
+  
